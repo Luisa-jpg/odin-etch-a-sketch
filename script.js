245f34310bef97a4dd99b617c44ffeb9bef43e06
Luisa-jpg <luisa.metzen@arcor.de>
2024-05-22 20:10:51 +0200

    Add RGB mode funcionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector(".canvas");
 let squares;
 let mode = "hover";
+let rgbStatus = "off";
 
 function grid(size = 16) {
     if (size > 100) size = 100;
@@ -29,13 +30,36 @@ function grid(size = 16) {
     
 function hoverEffect(e) {
     e.target.classList.remove("square-hover");
+    e.target.removeAttribute("style");
 };
 
-function randomRGB() {
+function randomRGBA() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const a = Math.round((Math.random() * 0.99 + 0.01) * 10) / 10; // added alpha because of optics, but can't be 0
+    return "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
+};
 
+function setRGBA (e) {
+    e.target.style.backgroundColor = randomRGBA();
+};
+
+function clear() {
+    for (let el of squares) {
+        el.classList.remove("square-hover");
+        el.removeAttribute("style");
+    };
 };
 
 const sizeBtn = document.querySelector("#sizebtn");
+const switchBtn = document.querySelector(".switch input");
+const hoverP = document.querySelector("#hover");
+const sketchP = document.querySelector("#sketch");
+const clearBtn = document.querySelector("#clearBtn");
+const rgbBtn = document.querySelector(".rgbCheck");
+
+hoverP.setAttribute("style", "color: plum");
 
 sizeBtn.addEventListener("click", () => {
     let sizeInput = prompt("Set your canvas size (up to 100): ", 16);
@@ -46,19 +70,11 @@ sizeBtn.addEventListener("click", () => {
     squares = grid(sizeInput);
 });
 
-
-const switchBtn = document.querySelector(".switch input");
-const hoverP = document.querySelector("#hover");
-const sketchP = document.querySelector("#sketch");
-
-hoverP.setAttribute("style", "color: plum");
-
 squares = grid(16);
 
 switchBtn.addEventListener("click", () => {
     if (mode === "hover") {
         for (let el of squares) {
-            el.classList.remove("square-hover");
             el.removeEventListener("mouseout", hoverEffect);
         };
         mode = "sketch";
@@ -66,7 +82,6 @@ switchBtn.addEventListener("click", () => {
         hoverP.setAttribute("style", "color: white");
     } else {
         for (let el of squares) {
-            el.classList.remove("square-hover");
             el.addEventListener("mouseout", hoverEffect);
         };
         mode = "hover";
@@ -75,3 +90,19 @@ switchBtn.addEventListener("click", () => {
     }
 });
 
+clearBtn.addEventListener("click", clear);
+
+rgbBtn.addEventListener("click", () => {
+    rgbBtn.classList.toggle("active-check");
+    if (rgbStatus === "on") {
+        for (let el of squares) {
+            el.removeEventListener("mouseover", setRGBA);
+        };
+        rgbStatus = "off";
+    } else {
+        for (let el of squares) {
+            el.addEventListener("mouseover", setRGBA);
+        };
+        rgbStatus = "on";
+    }
+});
